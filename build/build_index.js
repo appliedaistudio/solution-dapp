986d96219cf5a942bc08d2e8a5cb4c03e501fcc7
Jerry Overton <jerryaoverton@yahoo.com>
2023-11-03 11:09:55 -0500

    sdk

--- build/build_index.js
+++ build/build_index.js
@@ -1,33 +1,4 @@
-function navItemWrapper(nav_collapse_parent_show_id, nav_title) {
-    // get the nav item wrapper template html
-    var nav_item_wrapper = this.document.getElementById("nav-item-wrapper-template").content;
-    var nav_item_wrapper_HTML = this.document.importNode(nav_item_wrapper, true);
-
-    // place the nav collapse parent show id and title inside the template
-    nav_item_wrapper_HTML.querySelector(".navbar-vertical-label").textContent = nav_title;
-    nav_item_wrapper_HTML.querySelector(".nav.collapse.parent.show").id = nav_collapse_parent_show_id;
-
-    return nav_item_wrapper_HTML;
-}
-
-function navItem(nav_item_title) {
-    // get the nav item template html
-    var nav_item = this.document.getElementById("nav-item-template").content;
-    var nav_item_HTML = this.document.importNode(nav_item, true);
-
-    // place the nav title inside the template
-    nav_item_HTML.querySelector(".nav-link-text").textContent = nav_item_title;
-
-    return nav_item_HTML;
-}
-
-function placeNavItemIntoNavItemWrapper(nav_item_HTML, nav_collapse_parent_show_id) {
-    this.document.getElementById(nav_collapse_parent_show_id).appendChild(nav_item_HTML);
-}
-
-function placeNavItemWrapperIntoVerticalNavBar(nav_item_wrapper_HTML) {
-    this.document.getElementById("navbarVerticalNav").appendChild(nav_item_wrapper_HTML);
-}
+import { navItemWrapper, navItem, placeNavItemIntoNavItemWrapper, placeNavItemWrapperIntoVerticalNavBar } from "./build_sdk_index.js";
 
 function buildNav() {
     // create the home nav wrapper html
@@ -41,8 +12,7 @@ function buildNav() {
     placeNavItemIntoNavItemWrapper(home_nav_item_HTML, nav_collapse_parent_show_id="nv-home")
 }
 
-window.addEventListener("load", function() { 
-
+window.addEventListener("load", function() {
     buildNav();
 
 });
---
+++ build/build_sdk_index.js
@@ -0,0 +1,30 @@
+export function navItemWrapper(nav_collapse_parent_show_id, nav_title) {
+    // get the nav item wrapper template html
+    var nav_item_wrapper = this.document.getElementById("nav-item-wrapper-template").content;
+    var nav_item_wrapper_HTML = this.document.importNode(nav_item_wrapper, true);
+
+    // place the nav collapse parent show id and title inside the template
+    nav_item_wrapper_HTML.querySelector(".navbar-vertical-label").textContent = nav_title;
+    nav_item_wrapper_HTML.querySelector(".nav.collapse.parent.show").id = nav_collapse_parent_show_id;
+
+    return nav_item_wrapper_HTML;
+}
+
+export function navItem(nav_item_title) {
+    // get the nav item template html
+    var nav_item = this.document.getElementById("nav-item-template").content;
+    var nav_item_HTML = this.document.importNode(nav_item, true);
+
+    // place the nav title inside the template
+    nav_item_HTML.querySelector(".nav-link-text").textContent = nav_item_title;
+
+    return nav_item_HTML;
+}
+
+export function placeNavItemIntoNavItemWrapper(nav_item_HTML, nav_collapse_parent_show_id) {
+    this.document.getElementById(nav_collapse_parent_show_id).appendChild(nav_item_HTML);
+}
+
+export function placeNavItemWrapperIntoVerticalNavBar(nav_item_wrapper_HTML) {
+    this.document.getElementById("navbarVerticalNav").appendChild(nav_item_wrapper_HTML);
+}
