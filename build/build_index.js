d34994ead9037d8e8642ce7c350a05fa6b4ede6a
Jerry Overton <jerryaoverton@yahoo.com>
2023-11-04 15:02:56 -0500

    working on menu items

--- build/build_index.js
+++ build/build_index.js
@@ -1,15 +1,20 @@
-import { navItemWrapper, navItem, placeNavItemIntoNavItemWrapper, placeNavItemWrapperIntoVerticalNavBar } from "./build_sdk_index.js";
+import { navSection, placeNavSectionIntoVerticalNavBar, navItemsContainer, placeNavItemsContainerIntoNavSection} from "./build_sdk_index.js";
+import { navItem, placeNavItemIntoNavItemsContainer} from "./build_sdk_index.js";
 
 function buildNav() {
-    // create the home nav wrapper html
-    var home_nav_item_wrapper_HTML = navItemWrapper("nv-home", "brand new flava");
+    // create a nav section html
+    var nav_section_HTML = navSection("nav-section-1", "nav section 1");
 
-    // place the home nav wrapper into the vertical nav bar
-    placeNavItemWrapperIntoVerticalNavBar(home_nav_item_wrapper_HTML);
+    // place the nav section html into the vertical nav bar
+    placeNavSectionIntoVerticalNavBar(nav_section_HTML);
 
-    // create a home nav item and place it into the home nav wrapper
-    var home_nav_item_HTML = navItem("homeward bound");
-    placeNavItemIntoNavItemWrapper(home_nav_item_HTML, "nv-home")
+    // create a nav items container and place it into the nav section
+    var nav_items_container_HTML = navItemsContainer("nav-items-container-1", "nav items container");
+    placeNavItemsContainerIntoNavSection(nav_items_container_HTML, "nav-section-1")
+
+    // create a nav item and place it into the nav items container
+    var nav_item_HTML = navItem("nav item 1", "https://appliedai.studio");
+    placeNavItemIntoNavItemsContainer(nav_item_HTML, "nav-items-container-1");
 }
 
 window.addEventListener("load", function() {
--- build/build_sdk_index.js
+++ build/build_sdk_index.js
@@ -1,30 +1,52 @@
-export function navItemWrapper(nav_collapse_parent_show_id, nav_title) {
-    // get the nav item wrapper template html
-    var nav_item_wrapper = document.getElementById("nav-item-wrapper-template").content;
-    var nav_item_wrapper_HTML = document.importNode(nav_item_wrapper, true);
+export function navSection(nav_section_id, nav_section_title) {
+    // get the nav section template html
+    var nav_section_template = document.getElementById("nav-section-template").content;
+    var nav_section_HTML = document.importNode(nav_section_template, true);
 
-    // place the nav collapse parent show id and title inside the template
-    nav_item_wrapper_HTML.querySelector(".navbar-vertical-label").textContent = nav_title;
-    nav_item_wrapper_HTML.querySelector(".nav.collapse.parent.show").id = nav_collapse_parent_show_id;
+    // place the nav section id and title inside the template
+    nav_section_HTML.querySelector(".nav-item-wrapper").id = nav_section_id;
+    nav_section_HTML.querySelector(".navbar-vertical-label").textContent = nav_section_title;
 
-    return nav_item_wrapper_HTML;
+    return nav_section_HTML;
 }
 
-export function navItem(nav_item_title) {
-    // get the nav item template html
-    var nav_item = document.getElementById("nav-item-template").content;
-    var nav_item_HTML = document.importNode(nav_item, true);
+export function navItemsContainer(nav_items_container_id, nav_items_container_heading) {
+    // get the nav items container template html
+    var nav_items_container_template = document.getElementById("nav_items_container_template").content;
+    var nav_items_container_HTML = document.importNode(nav_items_container_template, true);
 
-    // place the nav title inside the template
+    // place the nav items container id and heading inside the template
+    nav_items_container_HTML.querySelector(".nav.collapse.parent").id = nav_items_container_id;
+    nav_items_container_HTML.querySelector(".nav-link.dropdown-indicator.label-1").href = "#" + nav_items_container_id;
+    nav_items_container_HTML.querySelector(".nav-link.dropdown-indicator.label-1").ariaControls = nav_items_container_id;
+    nav_items_container_HTML.querySelector(".nav-link-text").textContent = nav_items_container_heading;
+
+    return nav_items_container_HTML;
+}
+
+export function navItem(nav_item_title, nav_item_link) {
+    // get the nav item template html
+    var nav_item_template = document.getElementById("nav_item_template").content;
+    var nav_item_HTML = document.importNode(nav_item_template, true);
+
+    //place the nav item title and link inside the template
     nav_item_HTML.querySelector(".nav-link-text").textContent = nav_item_title;
+    nav_item_HTML.querySelector(".nav-link").href = nav_item_link;
 
     return nav_item_HTML;
 }
 
-export function placeNavItemIntoNavItemWrapper(nav_item_HTML, nav_collapse_parent_show_id) {
-    document.getElementById(nav_collapse_parent_show_id).appendChild(nav_item_HTML);
+export function placeNavSectionIntoVerticalNavBar(nav_section_HTML) {
+    document.getElementById("navbarVerticalNav").appendChild(nav_section_HTML);
+
+    //to do: replace the above with the following
+    //document.getElementById("navbarVerticalNav").replaceWith(nav_section_HTML);
 }
 
-export function placeNavItemWrapperIntoVerticalNavBar(nav_item_wrapper_HTML) {
-    document.getElementById("navbarVerticalNav").appendChild(nav_item_wrapper_HTML);
+export function placeNavItemsContainerIntoNavSection(nav_items_container_HTML, nav_section_id) {
+    document.getElementById(nav_section_id).replaceWith(nav_items_container_HTML);
+}
+
+export function placeNavItemIntoNavItemsContainer(nav_item_HTML, nav_items_container_id) {
+    document.getElementById(nav_items_container_id).appendChild(nav_item_HTML);
 }
