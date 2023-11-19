715552f710e9e3972a76f27e9565168080362c9f
Jerry Overton <jerryaoverton@yahoo.com>
2023-11-19 10:33:10 -0600

    started profile display

--- sdk/sdk_build_settings_options.js
+++ sdk/sdk_build_settings_options.js
@@ -2,7 +2,23 @@ const __author__ = "Jerry Overton"
 const __copyright__ = "Copyright (C) 2023 appliedAIstudio"
 const __version__ = "0.1"
 
+import {createIDfromTitleAndIndex} from "./sdk_build_utils.js"
 
 export function buildSettingsOptionsFromJSON(settings_options_json) {
-    //alert("build settings options");
+    // identify the profile information
+    let profile = settings_options_json['profile'];
+    let avatar_image = profile.avatar_image;
+    let username = profile.username;
+
+    buildProfileDisplay(avatar_image, username);
+
+    function buildProfileDisplay(avatar_image, username) {
+        // update the avatar image display
+        document.getElementById("avatarMenuImage").src = avatar_image;
+        document.getElementById("avatarDropdownImage").src = avatar_image;
+
+        // update the username
+        document.getElementById("username").textContent = username;
+    }
+
 }
