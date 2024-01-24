7c06b22313dcfa2c997fd43db604f49dd9c76be8
Jerry Overton <jerryaoverton@yahoo.com>
2024-01-24 08:39:27 -0600

    updated url ref error in main content controls

--- js/db/db-init-main-content-controls.js
+++ js/db/db-init-main-content-controls.js
@@ -2,7 +2,7 @@ import { putData, fetchJSONData } from './db-init-common.js';
 
 async function initializeMainContent(db) {
   // Fetch the JSON data and await its resolution to ensure contentControls is an array
-  const contentData = await fetchJSONData('../../data/main-content-controls.json');
+  const contentData = await fetchJSONData('./data/main-content-controls.json');
 
   // Put the retreived menu data into the database
   await putData(db, contentData);
