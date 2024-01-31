80b9973226e2b0bc81ceb0733689e3ae9a51545f
Jerry Overton <jerryaoverton@yahoo.com>
2024-01-31 12:28:45 -0600

    more url updates

--- js/db/db-init-menu.js
+++ js/db/db-init-menu.js
@@ -2,7 +2,7 @@ import { putData, fetchJSONData } from './db-init-common.js';
 
 async function initializeMenu(db) {
   // Fetch the JSON data and await its resolution to ensure contentControls is an array
-  const menuData = await fetchJSONData('/data/menu-options.json');
+  const menuData = await fetchJSONData('./data/menu-options.json');
 
   // Put the retreived menu data into the database
   await putData(db, menuData);
